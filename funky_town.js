aee304ce43e049a879354fb38fbff151f9b799c8
Jen Yu <jyu7@stuy.edu>
2017-12-06 13:10:50 -0500

    fib and gcd fxns working

---
+++ funky_town.js
@@ -0,0 +1,35 @@
+var fibonacci = function(n){
+    if(n == 0){return 0;}
+    else if (n == 1){return 1;}
+    else{return fibonacci(n-1) + fibonacci(n-2);}
+};
+
+var value = "" +  fibonacci(0); //-> 0
+value += " " + fibonacci(1); //-> 1
+value += " "+ fibonacci(2); //-> 1
+value += " "+ fibonacci(3); //-> 2
+value += " "+ fibonacci(4); //-> 3
+console.log(value);
+
+var gcd = function(a, b){
+    if (a == 0){return b;}
+    else if (b == 0){return a;}
+    else{
+	max = Math.max(a, b);
+	min = Math.min(a, b);
+	return gcd(min, max%min);
+    }
+}
+
+
+var value = "" +  gcd(36,6); //-> 6
+value += " " + gcd(6,36); //-> 6
+value += " "+ gcd(17,5); //-> 1
+value += " "+ gcd(200,5); //-> 5
+value += " "+ gcd(4, 0); //-> 4
+console.log(value);
+
+var randomStudent = function(list){
+    
+}
+
